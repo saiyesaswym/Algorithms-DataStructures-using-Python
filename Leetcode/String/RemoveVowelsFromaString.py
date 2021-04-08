e0bd544452aff675adbaad691d3b4e82f5381a5b
Sai Yesaswy <yesaswym@gmail.com>
2021-04-07 21:09:02 -0400

    remove vowels

---
+++ Leetcode/String/RemoveVowelsFromaString.py
@@ -0,0 +1,14 @@
+"""
+First approach: Brute force approach
+Looping through the string and finding for vowels
+Time complexity: O(n) ("in" operator for SET takes O(1))
+Run time: 28ms
+Faster than 78.99%
+"""
+def removeVowels(s: str) -> str:
+    vowels = {'a','e','i','o','u'}
+    ans=''
+    for i in s:
+        if i not in vowels:
+            ans+=i
+    return ans
